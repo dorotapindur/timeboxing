2012938da0067e707884a580c5bc3c945b6a0edf
Dorota Pindur <dpindur@wp.pl>
2020-12-22 18:59:22 +0100

    transfer App component to App.js

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { TimeboxList, EditableTimebox } from "./remaining-components";
+
+function App() {
+    return (
+        <div className="App">
+            <TimeboxList />
+            <EditableTimebox />
+        </div>
+    )
+}
+
+export default App;
